26d426259976404282c8a7ab58c225148885bea2
Alex Luecke <alex.james.luecke@gmail.com>
2018-04-21 14:15:33 -0700

    formatting

--- src/main.rs
+++ src/main.rs
@@ -60,29 +60,33 @@ fn main() {
 
     for record in get_records().unwrap() {
         let hour_minute: Vec<i32> = match record.duration {
-            Some(v) => v.split(":")
-                .take(2)
-                .map(|e| e.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>(),
+            Some(v) => {
+                v.split(":").take(2).map(|e| e.parse::<i32>().unwrap()).collect::<Vec<i32>>()
+            },
             None => {
-                println!("Error parsing duration field.");
+                print_msg("Duration", i);
                 std::process::exit(1);
             }
         };
 
         if hour_minute.len() != 2 {
-            println!("Duration field not formatted correctly for row: {}, value: {:?}", i, hour_minute);
+            print_msg("Duration", i);
             std::process::exit(1);
         }
 
-        let what = Bucket {
+        let time_bucket = Bucket {
             ticket: record.description.unwrap(),
             time: format!("{}h {}m", hour_minute[0], hour_minute[1])
         };
 
-        match date_map.data.entry(record.start_date.unwrap()) {
-            Entry::Occupied(mut entry) => { entry.get_mut().push(what); },
-            Entry::Vacant(entry) => { entry.insert(vec![what]); }
+        match record.start_date {
+            Some(key) => match date_map.data.entry(key) {
+                Entry::Occupied(mut entry) => { entry.get_mut().push(time_bucket); },
+                Entry::Vacant(entry) => { entry.insert(vec![time_bucket]); }
+            },
+            None => {
+                print_msg("Start Date", i); std::process::exit(1);
+            }
         }
 
         i += 1;
@@ -94,4 +98,8 @@ fn main() {
 fn get_records() -> Result<Vec<TogglRecord>, Box<Error>> {
     let file = fs::File::open("./files/toggl.csv").expect("File not found.");
     Ok(csv::Reader::from_reader(file).deserialize().filter_map(|r| r.ok()).collect())
+}
+
+fn print_msg(field: &str, row: i32) {
+    println!("Could not parse field {} for row {}.", field, row);
 }
